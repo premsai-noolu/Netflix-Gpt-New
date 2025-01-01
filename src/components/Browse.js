970f1a7f25c85c2d47356263d95f38c7aaebbcd1
premsai-noolu <premsainoolu01@gmail.com>
2025-01-02 03:54:15 +0530

    fetching trailer video and updating the store

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,22 +1,16 @@
 import React, { useEffect } from "react";
 import Header from "./Header";
-import { API_OPTIONS } from "../utils/constants";
+import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondryContainer from "./SecondryContainer";
 
 const Browse = () => {
-  const getNowPlayingMovies = async () => {
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing?page=1",
-      API_OPTIONS
-    );
-    const json = await data.json();
-    console.log(json.results);
-  };
-  useEffect(() => {
-    getNowPlayingMovies();
-  });
+  useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondryContainer />
     </div>
   );
 };
